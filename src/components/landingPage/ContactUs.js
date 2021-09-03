a8fee54a6fa61f35ee6a697d30ac7c8ba28d597f
mishrashree18 <mishrashree18@gmail.com>
2021-09-03 17:45:45 +0530

    :sparkles: Create landing page

---
+++ src/components/landingPage/ContactUs.js
@@ -0,0 +1,62 @@
+import Lottie from "lottie-react";
+import ContactAnimation from "../../images/ContactAnimation.json";
+
+function ContactUs() {
+  return (
+    <section className="text-gray-600 body-font relative sm:px-20">
+      <div className="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+        <div className="lg:w-2/3 md:w-1/2 rounded-lg overflow-hidden sm:mr-10 flex items-end justify-start relative">
+          <Lottie animationData={ContactAnimation} />
+        </div>
+        <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+          <h2 className="text-gray-900 text-lg mb-1 text-3xl font-bold title-font">
+            Feedback
+          </h2>
+          <p className="leading-relaxed mb-5 text-gray-600">
+            Share your feedback or you can contact us directly.
+          </p>
+          <div className="relative mb-4">
+            <label htmlFor="name" className="leading-7 text-sm text-gray-600">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="w-full bg-white rounded border border-gray-300 focus:border-primary focus:ring-2 focus:ring-primary text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label htmlFor="email" className="leading-7 text-sm text-gray-600">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="w-full bg-white rounded border border-gray-300 focus:border-primary focus:ring-2 focus:ring-primary text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+            />
+          </div>
+          <div className="relative mb-4">
+            <label
+              htmlFor="message"
+              className="leading-7 text-sm text-gray-600"
+            >
+              Message
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              className="w-full bg-white rounded border border-gray-300 focus:border-primary focus:ring-2 focus:ring-primary h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+            ></textarea>
+          </div>
+          <button className="text-white bg-primary border-0 py-2 px-6 focus:outline-none hover:opacity-80 rounded text-lg">
+            Submit
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default ContactUs;
---
+++ src/components/landingPage/WhyUs.js
@@ -0,0 +1,177 @@
+function Services() {
+  return (
+    <section className="text-gray-600 body-font bg-gray-100">
+      <div className="container px-5 py-24 mx-auto sm:px-20">
+        <h1 className="sm:text-4xl text-3xl font-bold title-font text-center text-gray-900 mb-20">
+          Why <span className="text-primary">Us?</span>
+        </h1>
+        <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+          <div className="p-4 md:w-1/3 flex">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-primary text-white mb-4 flex-shrink-0">
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="feather feather-home w-6 h-6"
+              >
+                <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+                <polyline points="9 22 9 12 15 12 15 22"></polyline>
+              </svg>
+            </div>
+            <div className="flex-grow pl-6">
+              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+                Virtual Waiting Room
+              </h2>
+              <p className="leading-relaxed text-base">
+                Freedom to wait for their turn privately in their car or
+                wherever they choose—rather than in medical halls
+              </p>
+            </div>
+          </div>
+          <div className="p-4 md:w-1/3 flex">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-primary text-white mb-4 flex-shrink-0">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="feather feather-slash"
+              >
+                <circle cx="12" cy="12" r="10"></circle>
+                <line x1="4.93" y1="4.93" x2="19.07" y2="19.07"></line>
+              </svg>
+            </div>
+            <div className="flex-grow pl-6">
+              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+                Germ-Free
+              </h2>
+              <p className="leading-relaxed text-base">
+                No confinement to a stuffy, crowded waiting room alongside new
+                germs and potential infections.
+              </p>
+            </div>
+          </div>
+          <div className="p-4 md:w-1/3 flex">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-primary text-white mb-4 flex-shrink-0">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+                className="feather feather-x-circle w-12 h-12"
+              >
+                <line x1="15" y1="9" x2="9" y2="15"></line>
+                <line x1="9" y1="9" x2="15" y2="15"></line>
+              </svg>
+            </div>
+            <div className="flex-grow pl-6">
+              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+                No rush or wait
+              </h2>
+              <p className="leading-relaxed text-base">
+                Facilitating a smooth check-in process for healthcare visits and
+                tests.
+              </p>
+            </div>
+          </div>
+          <div className="p-4 md:w-1/3 flex">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-primary text-white mb-4 flex-shrink-0">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                viewBox="0 0 24 24"
+                className="feather feather-users w-6 h-6"
+              >
+                <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
+                <circle cx="9" cy="7" r="4"></circle>
+                <path d="M23 21v-2a4 4 0 0 0-3-3.87"></path>
+                <path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+              </svg>
+            </div>
+            <div className="flex-grow pl-6">
+              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+                No standing in long Queues
+              </h2>
+              <p className="leading-relaxed text-base">
+                Automated Appointment Reminders to Patient and Pre-Appointment
+                and Pre-Arrival Instructions to Patients
+              </p>
+            </div>
+          </div>
+          <div className="p-4 md:w-1/3 flex">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-primary text-white mb-4 flex-shrink-0">
+              <svg
+                fill="none"
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                className="w-6 h-6"
+                viewBox="0 0 24 24"
+              >
+                <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+              </svg>
+            </div>
+            <div className="flex-grow pl-6">
+              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+                Secure and Fast
+              </h2>
+              <p className="leading-relaxed text-base">
+                Better privacy and security along with better user experience
+              </p>
+            </div>
+          </div>
+          <div className="p-4 md:w-1/3 flex">
+            <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-primary text-white mb-4 flex-shrink-0">
+              <svg
+                width="24"
+                height="22"
+                viewBox="0 0 24 22"
+                stroke="currentColor"
+                fill="none"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M12 21C17.5228 21 22 16.5228 22 11C22 5.47715 17.5228 1 12 1C6.47715 1 2 5.47715 2 11C2 16.5228 6.47715 21 12 21Z" />
+                <path d="M8 13C8 13 10.0062 12 12.2353 12C14.4644 12 16 13 16 13" />
+                <path d="M8 16C8 16 9.7709 18 12 18C14.2291 18 16 16 16 16" />
+                <path d="M9 8H9.01" />
+                <path d="M15 8H15.01" />
+                <path d="M16 16C16 16 16 15.3359 16 14.5C16 13.6641 16 13 16 13" />
+                <path d="M16 13C16 13 17.3282 12.5573 19 12C20.6718 11.4427 22 11 22 11" />
+                <path d="M2 11C2 11 3.32817 11.4427 5 12C6.67183 12.5573 8 13 8 13" />
+                <path d="M3 14C3 14 4.10681 14.4427 5.5 15C6.89319 15.5573 8 16 8 16" />
+                <path d="M16 16C16 16 17.1068 15.5573 18.5 15C19.8932 14.4427 21 14 21 14" />
+                <path d="M8 16C8 16 8 15.3359 8 14.5C8 13.6641 8 13 8 13" />
+              </svg>
+            </div>
+            <div className="flex-grow pl-6">
+              <h2 className="text-gray-900 text-lg title-font font-medium mb-2">
+                Social Distancing
+              </h2>
+              <p className="leading-relaxed text-base">
+                Contactless patient registration improves COVID-19 infection
+                control and practice social distancing norms
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Services;
