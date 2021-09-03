15cff2c0aa82ad6c0d51c322465a199cd7b4a0f4
Adyasha8105 <adyashamohanty8105@gmail.com>
2021-09-03 20:49:56 +0530

    :art: Add auth reducer to manage patient and doctor details

---
+++ src/redux/reducers/authReducer.js
@@ -0,0 +1,109 @@
+import { ActionTypes } from "../constants/actionTypes";
+
+const initialState = {
+  phoneno: "",
+  createdAt: "",
+  access: null,
+  id: null,
+  isLoginLoading: false,
+  isLoginError: "",
+  isSignupLoading: false,
+  isSignupError: "",
+  isRegistered: false,
+  isLoggedIn: false,
+  isLogoutErr: null,
+  isLogout: false,
+  type: "",
+  isSignedUp: false,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionTypes.CLEAN_STATE: {
+      return {
+        ...initialState,
+        isLoginError: "",
+      };
+    }
+    case ActionTypes.LOGIN_USER_START:
+      return {
+        ...state,
+        isLoginLoading: true,
+        isLoginError: "",
+      };
+
+    case ActionTypes.LOGIN_USER_SUCCESS: {
+      return {
+        ...state,
+        isLoginLoading: false,
+        phoneno: action.payload.phoneno || "",
+        access: action.payload.access || null,
+        id: action.payload.id || null,
+        isRegistered: action.payload.isRegistered,
+        isLoggedIn: action.payload.isLoggedIn,
+        isSignedUp: true,
+        type: action.payload.type,
+        isLoginError: "",
+        isLogout: false,
+      };
+    }
+
+    case ActionTypes.LOGIN_USER_FAILURE: {
+      return {
+        ...state,
+        isLoginLoading: false,
+        isLoginError: action.payload,
+      };
+    }
+
+    case ActionTypes.SIGNUP_USER_START: {
+      return {
+        ...state,
+        isSignupLoading: true,
+        isSignupError: "",
+      };
+    }
+    case ActionTypes.SIGNUP_USER_SUCCESS: {
+      console.log("message from signup reducer" + action.payload);
+      return {
+        ...state,
+        isLoginLoading: false,
+        phoneno: action.payload.phoneno || "",
+        access: action.payload.access || null,
+        id: action.payload.id || null,
+        isRegistered: action.payload.isRegistered,
+        isLoggedIn: action.payload.isLoggedIn,
+        type: action.payload.type,
+        isSignupLoading: false,
+        createdAt: action.payload.createdAt,
+        isSignedUp: true,
+        isSignupError: "",
+      };
+    }
+
+    case ActionTypes.SIGNUP_USER_FAILURE: {
+      return {
+        ...state,
+        isSignupLoading: false,
+        isSignupError: action.payload,
+      };
+    }
+    case ActionTypes.LOGOUT_SUCCESS: {
+      return {
+        ...initialState,
+        isLogout: true,
+      };
+    }
+    case ActionTypes.LOGOUT_FAILURE: {
+      return {
+        ...state,
+        isLogoutErr: action.payload,
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
