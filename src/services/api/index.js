5a63694913e90959e73d38743200ec2fe9c01b0e
Adyasha8105 <adyashamohanty8105@gmail.com>
2021-09-03 18:44:25 +0530

    :ambulance: Add services having all APIs

---
+++ src/services/api/index.js
@@ -0,0 +1,459 @@
+import {
+  SIGNUP_PATIENT_URL,
+  SIGNUP_DOCTOR_URL,
+  CREATE_AP_URL,
+  GET_AP_URL,
+  CANCEL_AP_URL,
+  CREATE_PATIENT_URL,
+  GET_ALL_DOC,
+  LOGIN_PATIENT_URL,
+  LOGIN_DOCTOR_URL,
+  CREATE_DOCTOR_URL,
+  LOGOUT_PATIENT_URL,
+  LOGOUT_DOCTOR_URL,
+  GET_DOCTOR,
+  GET_PATIENT,
+  UPDATE_PATIENT_URL,
+  UPDATE_DOCTOR_URL,
+  UPDATE_AP_URL,
+  SPARK_API_URL,
+} from "../../utils/url";
+
+export const SignupPatient = async (data) => {
+  console.log("SIGNUP DATA", data);
+  return new Promise(async (resolve, reject) => {
+    await fetch(`${SIGNUP_PATIENT_URL}`, {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("SIGNUP RESULT", result);
+        if (result.success) resolve(result.response);
+        else reject(result.err);
+      })
+      .catch((err) => reject(err));
+  });
+};
+
+export const SignupDoctor = async (data) => {
+  return new Promise(async (resolve, reject) => {
+    await fetch(`${SIGNUP_DOCTOR_URL}`, {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("SIGNUP DOCTOR RESULT", result);
+        if (result.success) resolve(result.response);
+        else reject(result.err);
+      })
+      .catch((err) => reject(err));
+  });
+};
+
+export const loginPatient = async (data) => {
+  console.log(data);
+  return new Promise(async (resolve, reject) => {
+    await fetch(`${LOGIN_PATIENT_URL}`, {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        if (result.success) resolve(result.response.data);
+        else reject(result.err);
+      })
+      .catch((err) => reject(err));
+  });
+};
+
+export const loginDoctor = async (data) => {
+  console.log(data);
+  return new Promise(async (resolve, reject) => {
+    await fetch(`${LOGIN_DOCTOR_URL}`, {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        if (result.success) resolve(result.response.data);
+        else reject(result.err);
+      });
+  });
+};
+
+export const createPatient = async (data) => {
+  const bodyObj = {
+    id: data.id,
+    name: data.name,
+    age: data.age,
+    city: data.city,
+    address: data.address,
+    state: data.state,
+    pincode: data.pincode,
+    dob: data.dob,
+    phoneNo: data.phone,
+    gender: data.gender,
+    bloodGp: data.bloodGp,
+    email: data.email,
+  };
+  console.log(bodyObj);
+  return new Promise(async (resolve, reject) => {
+    await fetch(`${CREATE_PATIENT_URL}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyObj),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        if (result.success) resolve(result.response);
+        else reject(result.err);
+      })
+      .catch((err) => reject(err));
+  });
+};
+
+export const updatePatient = async (data) => {
+  console.log("PAtient Update Data", data);
+
+  return new Promise(async (resolve, reject) => {
+    await fetch(`${UPDATE_PATIENT_URL}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.success) resolve(result.response);
+        else reject(result.err);
+      })
+      .catch((err) => {
+        reject(err.message);
+      });
+  });
+};
+
+export const updateDoctor = async (data) => {
+  console.log("Doctor Update Data", data);
+
+  return new Promise(async (resolve, reject) => {
+    await fetch(`${UPDATE_DOCTOR_URL}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.success) resolve(result.response);
+        else reject(result.err);
+      })
+      .catch((err) => {
+        reject(err.message);
+      });
+  });
+};
+
+export const createDoctor = async (data) => {
+  console.log("DOCTOR DATA", data);
+  return new Promise(async (resolve, reject) => {
+    await fetch(`${CREATE_DOCTOR_URL}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: data,
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        if (result.success) resolve(result.response);
+        else reject(result.err);
+      })
+      .catch((err) => reject(err));
+  });
+};
+
+export const logoutPatient = async (data) => {
+  return new Promise(async (resolve, reject) => {
+    await fetch(
+      `${LOGOUT_PATIENT_URL}?` +
+        new URLSearchParams({
+          phoneNo: data.phoneno,
+        }),
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${data.token}`,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.success) resolve(result.response);
+        else reject(result.err);
+      });
+  });
+};
+
+export const logoutDoctor = async (data) => {
+  return new Promise(async (resolve, reject) => {
+    await fetch(
+      `${LOGOUT_DOCTOR_URL}?` +
+        new URLSearchParams({
+          phoneNo: data.phoneno,
+        }),
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${data.token}`,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("LOGOUT DOCTOR", result);
+        if (result.success) resolve(result.response);
+        else reject(result.err);
+      });
+  });
+};
+
+export const getAllDoctors = async () => {
+  return new Promise(async (resolve, reject) => {
+    await fetch(`${GET_ALL_DOC}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("DOCTOR ALL ",result)
+        if(result.success)
+        resolve(result.response);
+        else
+        reject(result.err)
+      })
+      .catch((err) => reject(err));
+  });
+};
+
+export const getDoctor = async (phoneno) => {
+  console.log("IN DOCTOR PHONE", phoneno);
+  return new Promise(async (resolve, reject) => {
+    await fetch(
+      `${GET_DOCTOR}?` +
+        new URLSearchParams({
+          phoneno: phoneno,
+        }),
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.success) resolve(result.response);
+        else reject(result.err);
+      })
+      .catch((err) => reject(err.message));
+  });
+};
+
+export const getPatient = async (phoneno) => {
+  console.log("IN PATIENT PHONE", phoneno);
+  return new Promise(async (resolve, reject) => {
+    await fetch(
+      `${GET_PATIENT}?` +
+        new URLSearchParams({
+          phoneno: phoneno,
+        }),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        console.log("PatientDetails",result);
+        if (result.success) resolve(result.response);
+        else reject(result.err);
+      })
+      .catch((err) => reject(err.message));
+  });
+};
+
+export const createAppointment = async (data) => {
+  return new Promise(async (resolve, reject) => {
+    await fetch(`${CREATE_AP_URL}`, {
+      method: "POST",
+      body: data,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        if(result.success)
+        resolve(result);
+        else
+        reject(result.err)
+      })
+      .catch((err) => reject(err));
+  });
+};
+
+export const getAppointment = async (data) => {
+  return new Promise(async (resolve, reject) => {
+
+    console.log("GET APPOINTMENT",data)
+    if (data.forUser === "doctor") {
+      const url = data.status?GET_AP_URL +
+      `all?forUser=doctor&status=${data.status}&dphoneNo=${data.dphoneNo}`:
+      GET_AP_URL +
+          `all?forUser=doctor&dphoneNo=${data.dphoneNo}`
+      await fetch(
+        url,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          console.log(result)
+          if(result.success)
+          resolve(result);
+          else
+          reject(result.response)
+        })
+        .catch((err) => reject(err));
+    } else if(data.forUser === 'patient'){
+      await fetch(
+        GET_AP_URL + `all?pphoneNo=${data.pphoneNo}&forUser=patient&status=queued`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((result) => {
+      
+          if(result.success)
+          resolve(result);
+          else
+          reject(result.response)
+        })
+        .catch((err) => reject(err));
+    }
+    else return;
+  });
+};
+
+
+export const getTodayAppointment = async (data) => {
+  return new Promise(async (resolve,reject) => {
+
+    const url = GET_AP_URL +
+    `all?forUser=doctor&dphoneNo=${data.dphoneNo}`
+    await fetch(
+      url,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result)
+        if(result.success)
+        resolve(result);
+        else
+        reject(result.response)
+      })
+      .catch((err) => reject(err));
+
+  })
+}
+export const cancelAppointment = async (data) => {
+  return new Promise(async (resolve, reject) => {
+    await fetch(`${CANCEL_AP_URL}`, {
+      method: "DELETE",
+      body: data,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        if(result.success)
+        resolve(result);
+        else
+        reject(result.err)
+      })
+      .catch((err) => reject(err));
+  });
+};
+
+export const updateAppointment = async (data) => {
+  return new Promise(async (resolve,reject) => {
+    await fetch(`${UPDATE_AP_URL}`,{
+      method:"PUT",
+      body:data,
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then((result) => {
+      if(result.success)
+      resolve(result.response)
+      else
+      reject(result.err)
+    })
+    .catch(err => reject(err.message))
+  })
+}
+export const testRoute = async () => {
+  return new Promise(async (resolve,reject) => {
+    await fetch(`${SPARK_API_URL}`,{
+      method:'get',
+      headers:{
+        'Content-Type':'application/json'
+      }
+    })
+    .then(result => resolve(result))
+    .catch(err => reject(err))
+  })
+}
