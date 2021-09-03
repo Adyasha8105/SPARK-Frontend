24d8b1ea488a0dd8917206a322679bcd65e41b97
piyushpradhan <piyushpradhan3.14@gmail.com>
2021-09-03 20:55:35 +0530

    :white_check_mark: update reducers index file

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,13 +1,32 @@
 import { combineReducers } from "redux";
+import appointmentReducer from "./appointmentReducer";
+import profileReducer from "./profileReducer";
+import authReducer from "./authReducer";
+import doctorReducer from "./doctorReducer";
+import todayAppointmentReducer from "./todayAppointmentReducer";
+import patientReducer from "./patientsReducer";
 import { persistReducer } from "redux-persist";
-import storage from "redux-persist/storage";
+import storage from "redux-persist/lib/storage";
 
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: [],
+  whitelist: [
+    "profileReducer",
+    "appointmentReducer",
+    "authReducer",
+    "doctorReducer",
+    "patientReducer",
+  ],
 };
 
-const reducers = combineReducers({});
+const reducers = combineReducers({
+  appointmentReducer,
+  todayAppointmentReducer,
+  profileReducer,
+  authReducer,
+  doctorReducer,
+  patientReducer,
+});
 
 export default persistReducer(persistConfig, reducers);
