43067208353c087533069d9b38b411009dffddcb
piyushpradhan <piyushpradhan3.14@gmail.com>
2021-09-03 18:55:58 +0530

    Merge branch 'main' of github.com:Adyasha8105/SPARK-Frontend into main

---
+++ src/components/DoctorButton.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { BsArrowRepeat } from "react-icons/bs";
+import { FiFastForward } from "react-icons/fi";
+import { BiTimeFive } from "react-icons/bi";
+
+function WaitingListItemButton({ appointmentStatus, onClickFunc }) {
+  let buttonColor;
+  let buttonTextColor;
+  let buttonBorderColor;
+  if (appointmentStatus === "Next") {
+    buttonColor = "bg-red-400";
+    buttonTextColor = "text-white";
+    buttonBorderColor = "border-white";
+  } else if (appointmentStatus === "Queued") {
+    buttonColor = "bg-gray-400";
+    buttonTextColor = "text-white";
+    buttonBorderColor = "border-white";
+  } else if (appointmentStatus === "Ongoing") {
+    buttonColor = "bg-green-400";
+    buttonBorderColor = "border-white";
+    buttonTextColor = "text-white";
+  }
+
+  return (
+    <button
+      onClick={() => {
+        if (appointmentStatus === "Next") onClickFunc();
+      }}
+      className={`${buttonColor} ${buttonTextColor} outline-none px-4 py-2 mx-8 flex flex-row space-x-2 items-center border ${buttonBorderColor} rounded-md`}
+    >
+      <p>{appointmentStatus}</p>
+      {appointmentStatus === "Ongoing" ? (
+        <BsArrowRepeat />
+      ) : appointmentStatus === "Next" ? (
+        <FiFastForward />
+      ) : appointmentStatus === "Queued" ? (
+        <BiTimeFive />
+      ) : null}
+    </button>
+  );
+}
+
+export default WaitingListItemButton;
---
+++ src/components/DoctorList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import WaitingListItemButton from "./DoctorButton";
+
+function WaitingListItem({
+  serialNo,
+  time,
+  isUser,
+  appointmentStatus,
+  onClickFunc,
+}) {
+  return (
+    <div className="grid md:flex flex-row bg-gray-50 rounded-md py-3 items-center">
+      <div className="flex flex-row w-full">
+        <p className="pl-8 pr-4">{serialNo}</p>
+        <p className="pl-4">{time}</p>
+      </div>
+
+      <WaitingListItemButton
+        PaddingButton="mx-8"
+        appointmentStatus={appointmentStatus}
+        onClickFunc={onClickFunc}
+      />
+    </div>
+  );
+}
+
+export default WaitingListItem;
